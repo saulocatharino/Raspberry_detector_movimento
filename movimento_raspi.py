3cd39f17e81d6a341756ae5623e2630fc889dc78
Saulo Catharino <saulocatharino@gmail.com>
2018-09-19 10:04:33 -0300

    Update movimento_raspi.py

--- movimento_raspi.py
+++ movimento_raspi.py
@@ -21,7 +21,7 @@ camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
-print("foi") 
+#print("foi") 
 # allow the camera to warmup
 time.sleep(0.1)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -33,32 +33,34 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     fgmask = fgbg.apply(image)
     #contours
     im2, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    try:
-        cnt = contours[0]
-        cv2.drawContours(fgmask, contours, -1, (0, 255, 0), 3)
-    except:
-        pass
     if len(contours) > 0:
+        cnt = contours[0]
+        #cv2.drawContours(fgmask, contours, -1, (0, 255, 0), 3)
         # find largest contour in mask, use to compute minEnCircle
-        c = max(contours, key=cv2.contourArea)
-        (x, y), radius = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        x_ = int(x - radius)
-        y_ = int(y - radius)
-        h_ = int(radius * 2)
-        w_ = int(radius * 2)
 
+        for (i,contour) in enumerate (contours):
+            (x,y,w,h) = cv2.boundingRect(contour)
+            contour_valid = (w >= 40) and (h >= 40)
+            if contour_valid:
+                cv2.rectangle(image, (x,y), (x+w,y+h), (255,0,255),2)
+            if not contour_valid:
+                continue
 
-        sss = distance.euclidean((x_,y_),(x_ +w_,y_ +h_))
+        #c = max(contours, key=cv2.contourArea)
+        #(x, y), radius = cv2.minEnclosingCircle(c)
+        #M = cv2.moments(c)
+        #x_ = int(x - radius)
+        #y_ = int(y - radius)
+        #h_ = int(radius * 2)
+        #w_ = int(radius * 2)
 
-        if sss>30:
-            cv2.rectangle(image, (x_, y_), (x_ + w_, y_ + h_), (254, 255, 0), 2)
+        #sss = distance.euclidean((x_,y_),(x_ +w_,y_ +h_))
+
+        #if sss>30:
+            #cv2.rectangle(image, (x_, y_), (x_ + w_, y_ + h_), (254, 255, 0), 2)
 
     cv2.imshow('Camera', image)
     rawCapture.truncate(0)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
-
-
-
