ec23ed50d20252e16830da5b505ab3214c2e14b1
Saulo Catharino <saulocatharino@gmail.com>
2019-10-19 13:49:56 -0300

    Update movimento_raspi.py

--- movimento_raspi.py
+++ movimento_raspi.py
@@ -11,18 +11,12 @@ fgbg = cv2.createBackgroundSubtractorMOG2(history=20,
                                           detectShadows=False)
 
 
-
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+cap = cv2.VideoCapture(0)
 #print("foi") 
 # allow the camera to warmup
 time.sleep(0.1)
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-
-    image = rawCapture.array
-
+while True:
+    _, image = cap.read()
 
     fgmask = fgbg.apply(image)
     #contours
